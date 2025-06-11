1900b7eec79b7376288ad94981c718e84f10677c
Esteban Ugarte <esteban.ugarte@alumnos.ispi4038.edu.ar>
2025-06-11 16:54:58 -0300

    Se agrega elif de adolecente y adultos

--- practica.bash
+++ practica.bash
@@ -7,8 +7,12 @@ read -p"ingrese edad: " edad
 #edades= ($edad)
 echo $edad
 
-if [($edad <= 13)]; then
+if (($edad <= 13)); then
    echo 'es niño'
+elif (($edad <= 17)); then
+    echo 'es adolecente'
+elif (($edad <= 64)); then
+     echo 'es Adulto'
 fi
 
     
